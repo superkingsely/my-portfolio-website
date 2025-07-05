6b1529001d507ab3e17759b8e652954c7ae4644b
superkingsely <superkingsely@gmail.com>
2025-07-05 10:53:06 +0100

    okay

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,24 +1,6 @@
-// import React from 'react'
 
-// const Projects = () => {
-//   return (
-//     <div  className='min-h-[70vh] pt-[50px] flex flex-col ' id='projects' >
-//         <div className="h1 flex flex-col items-center text-3xl font-bold  ">
-//             <span>
-//             My Awesome 
-//             </span>
-//             <span>Projects</span>
-//         </div>
-//         <div className="proj-block flex-1 border  flex justify-center items-center font-extrabold text-[32px] ">
-//             <span>Incomming...</span>
-//         </div>
-//     </div>
-//   )
-// }
 
-// export default Projects
-
-"use client"; // for client-side only behavior in Next.js 13+
+"use client";
 
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
